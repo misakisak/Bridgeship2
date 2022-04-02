11f8712c184facae2158f6308f48076bbd83d2a1
misakisak <dr.misakisak@gmail.com>
2022-04-02 18:40:31 -0400

    adding bottom tab navigator

--- App.js
+++ App.js
@@ -32,7 +32,7 @@ import LandingScreen from './components/auth/Landing';
 import RegisterScreen from './components/auth/Register';
 import MainScreen from './components/Main';
 import LoginScreen from './components/auth/Login'
-
+import AddScreen from './components/main/Add';
 
 const Stack = createStackNavigator();
 
@@ -93,9 +93,14 @@ export class App extends Component {
 
     return(
       <Provider store={store}>
-        <MainScreen />
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Main">
+              <Stack.Screen name="Main" component={MainScreen} options={{headerShown: false}}/>
+              <Stack.Screen name="Add" component={AddScreen}/>
+          </Stack.Navigator>
+        </NavigationContainer>
+
       </Provider>
-      
       
     )
   }
--- components/Main.js
+++ components/Main.js
@@ -1,27 +1,54 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchUser } from '../redux/actions/index';
+// import { TabActions } from '@react-navigation/native';
+
+import FeedScreen from './main/Feed';
+// import AddScreen from './main/Add';
+import ProfileScreen from './main/Profile';
+
+const Tab = createMaterialBottomTabNavigator
+
+const EmptyScreen = () => {
+    return(null)
+}
 
 export class Main extends Component {
     componentDidMount(){
         this.props.fetchUser();
-
     }
     render() {
-        const { currentUser } = this.props;
-        console.log()
-        if(currentUser==undefined){
-            return(
-                <View></View>
-            )
-        }
-        return (
-           <View style={{ flex: 1, justifyContent: 'center'}}>
-              <Text>{currentUser.name} is logged in</Text>
-           </View>
+        return ( 
+           <Tab.Navigator initialRouteName="Feed" labeled={false}>
+               <Tab.Screen name="Feed" component={FeedScreen}
+                    options={{
+                       tabBarIcon: ({ color, size }) => (
+                           <MaterialCommunityIcons name="home" color={color} size={26}/>
+                        ),
+                    }}/>
+               <Tab.Screen name="AddContainer" component={EmptyScreen}
+                    listeners={({ navigation }) => ({
+                        tabPress: event => {
+                            event.preventDefault();
+                            navigation.navigate("Add")
+                        }
+                    })}
+                    options={{
+                       tabBarIcon: ({ color, size }) => (
+                           <MaterialCommunityIcons name="plus-box" color={color} size={26}/>
+                        ),
+                    }}/>
+               <Tab.Screen name="Profile" component={ProfileScreen}
+                    options={{
+                       tabBarIcon: ({ color, size }) => (
+                           <MaterialCommunityIcons name="account-circle" color={color} size={26}/>
+                        ),
+                    }}/>
+           </Tab.Navigator>
         )
     }
 }
